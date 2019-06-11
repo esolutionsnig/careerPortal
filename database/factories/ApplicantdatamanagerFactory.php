d232167a4a1865164f769f8a898aaa902e6239c4
Ernest Ibeh <ibeh_ernest@yahoo.com>
2019-06-11 15:13:54 +0100

    Created Model -a Applicantdatamanager

---
+++ database/factories/ApplicantdatamanagerFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+/* @var $factory \Illuminate\Database\Eloquent\Factory */
+
+use App\Model\Applicantdatamanager;
+use Faker\Generator as Faker;
+
+$factory->define(Applicantdatamanager::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
