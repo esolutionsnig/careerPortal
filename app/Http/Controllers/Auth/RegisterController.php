c61d4d8d2dbdf8ab1b1a3f15e93a54433fe86e79
Ernest Ibeh <ibeh_ernest@yahoo.com>
2019-06-14 12:16:28 +0100

    Passport Installed

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -49,7 +49,9 @@ class RegisterController extends Controller
     protected function validator(array $data)
     {
         return Validator::make($data, [
-            'name' => ['required', 'string', 'max:255'],
+            'surname' => ['required', 'string', 'max:100'],
+            'firstname' => ['required', 'string', 'max:100'],
+            'othernames' => ['required', 'string', 'max:100'],
             'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
             'password' => ['required', 'string', 'min:8', 'confirmed'],
         ]);
@@ -64,7 +66,9 @@ class RegisterController extends Controller
     protected function create(array $data)
     {
         return User::create([
-            'name' => $data['name'],
+            'surname' => $data['surname'],
+            'firstname' => $data['firstname'],
+            'othernames' => $data['othernames'],
             'email' => $data['email'],
             'password' => Hash::make($data['password']),
         ]);
